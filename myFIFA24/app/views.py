d85119753c6c8d6c6f114b3000b16744e7bdd387
Guilherme Antunes <guicostaantunes@gmail.com>
2024-04-12 09:21:31 +0100

    change api folder

--- myFIFA24/app/views.py
+++ myFIFA24/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from myFIFA24.api.leagues import get_leagues, get_leagues_by_name, get_league_by_guid
+from .api import leagues as leagues_api
 # Create your views here.
 
 
@@ -8,15 +8,15 @@ def index(request):
 
 
 def leagues_view(request):
-    leagues = get_leagues()
+    leagues = leagues_api.get_leagues()
     return render(request, 'leagues.html', {'leagues': leagues})
 
 def leagues_by_name_view(request, name):
-    leagues = get_leagues_by_name(name)
+    leagues = leagues_api.get_leagues_by_name(name)
     return render(request, 'leagues_by_name.html', {'leagues': leagues})
 
 def league_by_guid_view(request, guid):
-    league = get_league_by_guid(guid)
+    league = leagues_api.get_league_by_guid(guid)
     return render(request, 'league_by_guid.html', {'league': league})
 
 
